41b4e90e4483ada9858456fd3613e1bad0482caa
Chadwick Dahlquist <chadwick@bugeats.net>
2018-08-04 03:02:07 -0700

    initial xmonad setup

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,16 @@
+import XMonad
+import XMonad.Layout.Spacing
+
+main :: IO ()
+main = do
+  xmonad $ defaultConfig
+    { borderWidth = 1
+    , terminal    = "alacritty"
+    , focusedBorderColor    = "#645851"
+    , normalBorderColor    = "#272524"
+    , layoutHook = spacingWithEdge mySpacing $ Tall 1 (3/100) ratio
+    }
+  where
+    ratio = toRational (2/(1+sqrt(5)::Double)) -- golden ratio
+
+mySpacing = 16
