27c73da0e1dbc1511e3b8f7b638a8b10d0a2dfbd
Chadwick Dahlquist <chadwick@bugeats.net>
2018-08-04 03:06:09 -0700

    xmonad format tweaks

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,11 +4,11 @@ import XMonad.Layout.Spacing
 main :: IO ()
 main = do
   xmonad $ defaultConfig
-    { borderWidth = 1
-    , terminal    = "alacritty"
-    , focusedBorderColor    = "#645851"
-    , normalBorderColor    = "#272524"
-    , layoutHook = spacingWithEdge mySpacing $ Tall 1 (3/100) ratio
+    { borderWidth        = 1
+    , terminal           = "alacritty"
+    , focusedBorderColor = "#645851"
+    , normalBorderColor  = "#272524"
+    , layoutHook         = spacingWithEdge mySpacing $ Tall 1 (3/100) ratio
     }
   where
     ratio = toRational (2/(1+sqrt(5)::Double)) -- golden ratio
