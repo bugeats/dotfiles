30c654a42b349aca7a9eca43cbf7975af85c846f
Chadwick Dahlquist <chadwick@bugeats.net>
2018-08-04 06:02:18 -0700

    xmonad before three col layout

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,5 +1,6 @@
 import XMonad
 import XMonad.Layout.Spacing
+import XMonad.Layout.ThreeColumns
 
 main :: IO ()
 main = do
@@ -8,9 +9,9 @@ main = do
     , terminal           = "alacritty"
     , focusedBorderColor = "#645851"
     , normalBorderColor  = "#272524"
-    , layoutHook         = spacingWithEdge mySpacing $ Tall 1 (3/100) ratio
+    , layoutHook         = myLayout
     }
-  where
-    ratio = toRational (2/(1+sqrt(5)::Double)) -- golden ratio
 
+ratio = toRational (2/(1+sqrt(5)::Double)) -- golden ratio
 mySpacing = 16
+myLayout = spacingWithEdge mySpacing $ Tall 1 (3/100) ratio
