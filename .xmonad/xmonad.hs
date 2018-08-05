174ccbc94beff659b5d6c2690c429a43ee33595f
Chadwick Dahlquist <chadwick@bugeats.net>
2018-08-04 18:25:38 -0700

    xmonad mod-x to lock the screen

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,8 @@
 import XMonad
 import XMonad.Layout.Spacing
 import XMonad.Layout.ThreeColumns
+import XMonad.Util.EZConfig ( additionalKeys )
+import qualified Data.Map as M
 
 main :: IO ()
 main = do
@@ -10,8 +12,12 @@ main = do
     , focusedBorderColor = "#645851"
     , normalBorderColor  = "#272524"
     , layoutHook         = myLayout
-    }
+    } `additionalKeys` myKeys
 
-ratio = toRational (2/(1+sqrt(5)::Double)) -- golden ratio
+goldenRatio = toRational (2/(1+sqrt(5)::Double)) -- golden ratio
+
 mySpacing = 16
-myLayout = spacingWithEdge mySpacing $ Tall 1 (3/100) ratio
+
+myLayout = spacingWithEdge mySpacing $ ThreeColMid 1 (1/12) goldenRatio
+
+myKeys = [ ((mod1Mask, xK_x), spawn "slock") ]
