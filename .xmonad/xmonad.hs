cbf6dd2b8519894f8fc1fad8e8c58e12bb298d18
Chadwick Dahlquist <chadwick@bugeats.net>
2018-08-07 16:56:24 -0700

    xinit

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,8 @@
 import XMonad
 import XMonad.Layout.Spacing
 import XMonad.Layout.ThreeColumns
+import XMonad.Prompt
+import XMonad.Prompt.Shell
 import XMonad.Util.EZConfig ( additionalKeys )
 import qualified Data.Map as M
 
@@ -8,16 +10,30 @@ main :: IO ()
 main = do
   xmonad $ defaultConfig
     { borderWidth        = 1
+    , modMask            = mod4Mask -- Use Super instead of Alt
     , terminal           = "alacritty"
     , focusedBorderColor = "#645851"
     , normalBorderColor  = "#272524"
     , layoutHook         = myLayout
     } `additionalKeys` myKeys
 
+windowMargin = 16
 goldenRatio = toRational (2/(1+sqrt(5)::Double)) -- golden ratio
 
-mySpacing = 16
+myLayout = spacingWithEdge windowMargin $ ThreeColMid 1 (1/12) goldenRatio
 
-myLayout = spacingWithEdge mySpacing $ ThreeColMid 1 (1/12) goldenRatio
+myKeys = [ ((mod4Mask, xK_x), spawn "slock")
+         -- TODO shellPrompt isn't doing anything
+         , ((mod4Mask, xK_o), shellPrompt myXPConfig)
+         ]
 
-myKeys = [ ((mod1Mask, xK_x), spawn "slock") ]
+myXPConfig = defaultXPConfig
+             { font              = "-misc-fixed-*-*-*-*-12-*-*-*-*-*-*-*"
+             , bgColor           = "grey22"
+             , fgColor           = "grey80"
+             , fgHLight          = "black"
+             , bgHLight          = "grey"
+             , borderColor       = "white"
+             , position          = Top
+             , height            = 32
+             }
