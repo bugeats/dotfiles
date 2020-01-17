0e84ad21fdad72b034c942a4a113a1cca53d4095
Chadwick Dahlquist <cdahlquist@lo3energy.com>
2020-01-16 18:55:34 -0800

    Show 'grid select' in xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,46 +1,45 @@
 import XMonad
+import XMonad.Config.Xfce
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Hidden
 import XMonad.Layout.Spacing
 import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Gaps
 import XMonad.Prompt
 import XMonad.Prompt.Shell
 import XMonad.Util.EZConfig ( additionalKeys )
+import XMonad.Util.Replace ( replace )
+import XMonad.Actions.GridSelect
 import qualified Data.Map as M
+
+windowMargin = 6
+
+goldenRatio = toRational (2/(1+sqrt(5)::Double)) -- golden ratio
+
+myLayout = gaps [(D, 32 + windowMargin)] $ spacingWithEdge windowMargin $ hiddenWindows $ ThreeColMid 1 (1/24) goldenRatio
+
+myKeys = [ ((mod4Mask .|. shiftMask, xK_backslash), popOldestHiddenWindow)
+         , ((mod4Mask .|. shiftMask, xK_q), spawn "xfce4-session-logout")
+         , ((mod4Mask, xK_b), sendMessage ToggleStruts)
+         , ((mod4Mask, xK_backslash), withFocused hideWindow)
+         , ((mod4Mask, xK_g), sendMessage ToggleGaps)
+         , ((mod4Mask, xK_p), spawn "xfce4-popup-whiskermenu")
+         , ((mod4Mask, xK_s), spawn "systemctl suspend & slock")
+         -- , ((mod4Mask, xK_w), kill)
+         , ((mod4Mask, xK_x), spawn "slock")
+         , ((mod4Mask, xK_0), goToSelected defaultGSConfig)
+         ]
 
 main :: IO ()
 main = do
-  xmonad $ defaultConfig
+  replace
+  xmonad $ ewmh xfceConfig
     { borderWidth        = 6
     , modMask            = mod4Mask -- Use Super instead of Alt
-    , terminal           = "alacritty"
+    , terminal           = "alacritty -e tmux new-session -A -s SCRATCH"
     , focusedBorderColor = "#a57b55"
     , normalBorderColor  = "#272524"
     , layoutHook         = myLayout
     } `additionalKeys` myKeys
 
-windowMargin = 6
-goldenRatio = toRational (2/(1+sqrt(5)::Double)) -- golden ratio
-
-myLayout = spacingWithEdge windowMargin $ hiddenWindows $ ThreeColMid 1 (1/24) goldenRatio
-
--- myLayout = hiddenWindows (Tall 1 (3/100) (1/2)) ||| Full ||| etc..
--- main = xmonad def { layoutHook = myLayout }
-
-myKeys = [ ((mod4Mask, xK_x), spawn "slock")
-         , ((mod4Mask, xK_s), spawn "systemctl suspend & slock")
-         , ((mod4Mask, xK_backslash), withFocused hideWindow)
-         , ((mod4Mask .|. shiftMask, xK_backslash), popOldestHiddenWindow)
-         -- TODO shellPrompt isn't doing anything
-         , ((mod4Mask, xK_o), shellPrompt myXPConfig)
-         ]
-
-myXPConfig = defaultXPConfig
-             { font              = "-misc-fixed-*-*-*-*-12-*-*-*-*-*-*-*"
-             , bgColor           = "grey22"
-             , fgColor           = "grey80"
-             , fgHLight          = "black"
-             , bgHLight          = "grey"
-             , borderColor       = "white"
-             , position          = Top
-             , height            = 32
-             }
