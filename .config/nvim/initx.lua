740c12d2ccad26da3b482d584ea51ca12fc085d2
Chadwick Dahlquist <chadwick@bugeats.net>
2021-05-28 16:10:23 -0700

    Moar neovim lua hacking

--- .config/nvim/initx.lua
+++ .config/nvim/initx.lua
@@ -17,6 +17,10 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+-- Core ------------------------------------------------------------------------
+
+vim.g.mapleader = " " -- use spacebar for leader!
+
 
 -- Plugins ---------------------------------------------------------------------
 
@@ -25,6 +29,8 @@ require('packer').startup(function ()
   use 'eddyekofo94/gruvbox-flat.nvim'
 
   use 'mkitt/tabline.vim' -- complete and succinct tabline config
+
+  use 'sunjon/shade.nvim' -- shade inactive windows
 
   use {
     -- helpers to use native language server client
@@ -36,11 +42,34 @@ require('packer').startup(function ()
 
   use 'nvim-treesitter/playground'
 
+  use {
+    -- killer fuzzy finder
+    'nvim-telescope/telescope.nvim',
+    requires = {
+      'nvim-lua/popup.nvim',
+      'nvim-lua/plenary.nvim'
+    }
+  }
+
   use 'hrsh7th/nvim-compe' -- autocomplete in lua for LSP, buffers, and snippets
 
   use {
     'hoob3rt/lualine.nvim',
     requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
+
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true}
+
+  }
+
+  use {
+    -- git line status in gutter, among other things
+    'lewis6991/gitsigns.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    }
   }
 end)
 
@@ -61,6 +90,7 @@ cmd('set list')                                       -- Show whitespace charact
 cmd('set listchars=tab:▸\\ ,eol:¬')                   -- Use same symbols as TextMate for tabstops & EOLs
 
 cmd('set clipboard=unnamedplus')                      -- Support OS clipboard
+cmd('set signcolumn=yes')                             -- Always show the signcolumn to avoid jumpy rendering
 
 
 -- Treesitter ------------------------------------------------------------------
@@ -193,6 +223,101 @@ require'lspconfig'.sumneko_lua.setup {
 require'lspconfig'.tsserver.setup{}
 
 
+-- Telescope -------------------------------------------------------------------
+-- https://github.com/nvim-telescope/telescope.nvim
+
+cmd("nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown({}))<cr>")
+cmd("nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>")
+cmd("nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>")
+cmd("nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>")
+cmd("nnoremap <leader>ft <cmd>lua require('telescope.builtin').treesitter()<cr>")
+
+
+-- Nvim Tree -------------------------------------------------------------------
+
+vim.g.nvim_tree_git_hl = 1 -- enable file highlight for git attributes
+
+cmd("nnoremap <leader>d :NvimTreeToggle<cr>")
+
+
+-- Misc Leader -----------------------------------------------------------------
+
+-- nnoremap <leader>wf                    :only<cr>:NERDTreeFind<cr>:wincmd l<cr>:GoldenViewResize<cr>
+-- nnoremap <leader>wF                    :only<cr>:NERDTreeFind<cr>:wincmd l<cr>:Goyo<cr>
+
+local function leader(key, cmd)
+  local keyX = '<leader>' .. key
+  local cmdX = cmd .. '<cr>'
+  vim.api.nvim_buf_set_keymap(0, 'n', keyX, cmdX, {noremap = true, silent = true})
+end
+
+leader('ld', '<cmd>lua vim.lsp.buf.definition()')
+leader('la', '<cmd>lua vim.lsp.buf.code_action()')
+leader('lr', '<cmd>lua vim.lsp.buf.rename()')
+
+leader('bb', ':bprevious') -- buffer back
+
+cmd('nnoremap K <cmd>lua vim.lsp.buf.hover()<CR>')
+
+---- window ----
+
+-- resize window with arrow keys
+cmd("nmap <silent><left>  :3wincmd ><cr>")
+cmd("nmap <silent><right> :3wincmd <<cr>")
+cmd("nmap <silent><down>  :3wincmd +<cr>")
+cmd("nmap <silent><up>    :3wincmd -<cr>")
+
+cmd("nnoremap <leader>wh :wincmd h<cr>")
+cmd("nnoremap <leader>wj :wincmd j<cr>")
+cmd("nnoremap <leader>wk :wincmd k<cr>")
+cmd("nnoremap <leader>wl :wincmd l<cr>")
+cmd("nnoremap <leader>wm :only<cr>") -- window maximize
+cmd("nnoremap <leader>wq :wincmd q<cr>") -- window quit
+cmd("nnoremap <leader>wr :wincmd r<cr>") -- window rotate
+cmd("nnoremap <leader>ws :wincmd s<cr>:wincmd j<cr>")
+cmd("nnoremap <leader>wv :wincmd v<cr>:wincmd l<cr>")
+
+---- tab ----
+
+cmd("nnoremap <leader>1 :tabn 1<CR>")
+cmd("nnoremap <leader>2 :tabn 2<CR>")
+cmd("nnoremap <leader>3 :tabn 3<CR>")
+cmd("nnoremap <leader>4 :tabn 4<CR>")
+cmd("nnoremap <leader>5 :tabn 5<CR>")
+cmd("nnoremap <leader>6 :tabn 6<CR>")
+cmd("nnoremap <leader>7 :tabn 7<CR>")
+cmd("nnoremap <leader>8 :tabn 8<CR>")
+cmd("nnoremap <leader>9 :tabn 9<CR>")
+
+cmd("nnoremap <leader>tq :tabclose<CR>") -- tab quit
+cmd("nnoremap <leader>th :tabnext<CR>")
+cmd("nnoremap <leader>tj :tabnext<CR>")
+cmd("nnoremap <leader>tk :tabprev<CR>")
+cmd("nnoremap <leader>tl :tabprev<CR>")
+cmd("nnoremap <leader>tn :tabnew<CR>")
+
+cmd("nnoremap <leader>tf :tabm 0<CR>") -- tab floor - move the current tab all the way left
+
+
+-- Gitsigns --------------------------------------------------------------------
+
+require('gitsigns').setup {
+  signs = {
+    add          = {hl = 'GitSignsAdd'   , text = '+', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+    change       = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    delete       = {hl = 'GitSignsDelete', text = '-', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    topdelete    = {hl = 'GitSignsDelete', text = '-', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+  },
+  keymaps = {} -- no keymaps please
+}
+
+
 -- Color Scheme ----------------------------------------------------------------
+
+require'shade'.setup({
+  overlay_opacity = 50,
+  opacity_step = 1
+})
 
 cmd 'colorscheme gruvbox-flat'
